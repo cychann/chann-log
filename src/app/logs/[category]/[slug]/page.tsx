348c33e0533cfbe04bd6ba1f458a4e78e0ccc7b2
cychann <skcbdlhc129@naver.com>
2025-01-07 15:21:06 +0900

    [feat]: Log 관련 페이지 개발
    - logs, logs/[category], logs/[category]/[slug] 페이지 제작
    - LogList, LogCard 컴포넌트 제작

---
+++ src/app/logs/[category]/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import CustomMDX from "@/components/mdx/CustomMDX";
+import PostContent from "@/components/post/PostContent";
+import PostHeader from "@/components/post/PostHeader";
+import { getPostDetail } from "@/lib/post";
+
+interface Props {
+  params: {
+    category: string;
+    slug: string;
+  };
+}
+
+export default async function page({ params }: Props) {
+  const { category, slug } = await params;
+  const post = await getPostDetail("logs", category, decodeURIComponent(slug));
+
+  return (
+    <section className="mx-auto w-full max-w-[750px] my-5">
+      <PostHeader
+        title={post.title}
+        date={post.date}
+        category={post.category}
+        readingMinutes={post.readingMinutes}
+      />
+      <PostContent content={post.content} />
+    </section>
+  );
+}
