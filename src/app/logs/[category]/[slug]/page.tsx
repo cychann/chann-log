cba16936b0124b74c39480cd2e48d6dee4501bd5
cychann <skcbdlhc129@naver.com>
2025-02-04 16:19:01 +0900

    [feat]: 게시글 상세페이지 SEO 적용 및 구조 수정
    - title, description, images 적용 (추후에 배포 후, url 수정 필요)
    - slug 페이지에서 데이터 fetching 하도록 구조 수정

--- src/app/logs/[category]/[slug]/page.tsx
+++ src/app/logs/[category]/[slug]/page.tsx
@@ -1,19 +1,42 @@
 import PostDetailPage from "@/components/post/PostDetailPage";
+import { parseToc } from "@/lib/markdonw";
+import { getArticleDetail } from "@/lib/posts/article";
+import { getLogDetail } from "@/lib/posts/log";
+import { Metadata } from "next";
 
-interface Props {
+type LogDetailPageProps = {
   params: {
     category: string;
     slug: string;
   };
+};
+
+export async function generateMetadata({
+  params: { category, slug },
+}: LogDetailPageProps): Promise<Metadata> {
+  const post = await getLogDetail(category, decodeURIComponent(slug));
+
+  const imageURL = `/${post.thumbnail}`;
+
+  return {
+    title: post.title,
+    description: post.description,
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      images: [imageURL],
+    },
+  };
 }
 
-export default function logDetailpage({ params: { category, slug } }: Props) {
+export default async function logDetailpage({
+  params: { category, slug },
+}: LogDetailPageProps) {
+  const post = await getLogDetail(category, decodeURIComponent(slug));
+  const toc = await parseToc(post.content);
+
   return (
     /* @ts-expect-error Async Server Component */
-    <PostDetailPage
-      category={category}
-      slug={decodeURIComponent(slug)}
-      type="logs"
-    />
+    <PostDetailPage post={post} toc={toc} />
   );
 }
