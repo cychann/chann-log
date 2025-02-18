4abbd9ac2fa020c2f01cf1801f40a59a30340499
cychann <skcbdlhc129@naver.com>
2025-02-18 23:56:50 +0900

    [fix]: 변경된 디자인에 맞게 Skeleton UI 수정

--- src/components/loading/article/ArticleCardSkeleton.tsx
+++ src/components/loading/article/ArticleCardSkeleton.tsx
@@ -2,22 +2,16 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export function ArticleCardSkeleton() {
   return (
-    <div className="flex flex-col h-full rounded-lg border border-border bg-background-secondary shadow-md overflow-hidden">
-      <div className="relative aspect-video w-full">
-        <Skeleton className="absolute inset-0" />
-      </div>
-
-      <div className="flex flex-col flex-1 p-4 space-y-4">
+    <div className="flex justify-between py-6">
+      <div className="flex flex-col max-w-[calc(100%-144px)]">
+        <Skeleton className="h-[20px] w-3/4 mb-[6px]" />
         <div className="space-y-2">
-          <Skeleton className="h-7 w-3/4" />
-          <div className="space-y-2">
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-2/3" />
-          </div>
+          <Skeleton className="h-[15px] w-full" />
+          <Skeleton className="h-[15px] w-4/5" />
         </div>
 
-        <div className="space-y-3">
-          <div className="flex flex-wrap gap-2">
+        <div className="mt-auto pt-4 flex flex-col gap-[15px]">
+          <div className="flex gap-2">
             <Skeleton className="h-6 w-16 rounded-full" />
             <Skeleton className="h-6 w-16 rounded-full" />
             <Skeleton className="h-6 w-16 rounded-full" />
@@ -25,9 +19,13 @@ export function ArticleCardSkeleton() {
 
           <div className="flex items-center gap-2">
             <Skeleton className="h-4 w-24" />
-            <Skeleton className="h-4 w-20" />
+            <Skeleton className="h-4 w-16" />
           </div>
         </div>
+      </div>
+
+      <div className="relative w-32 h-24 self-start">
+        <Skeleton className="w-full h-full rounded-md" />
       </div>
     </div>
   );
--- src/components/loading/main/RecentArticleSkeleton.tsx
+++ src/components/loading/main/RecentArticleSkeleton.tsx
@@ -8,7 +8,7 @@ export function RecentArticleSkeleton() {
         <Skeleton className="h-7 w-32" />
         <Skeleton className="h-5 w-16" />
       </div>
-      <ul className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      <ul className="flex flex-col">
         {[...Array(3)].map((_, i) => (
           <li key={i}>
             <ArticleCardSkeleton />
